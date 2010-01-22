a3697de239fb9fd045bf41163f1906c6959440be
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-01-22 19:06:08 +0000

    2010-01-22	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* Makefile: Add sys/kd.h and sys/vt.h stubs.
    	* sys/kd.h, sys/vt.h: New files.

---
+++ mmap_kit/sys/vt.h
@@ -0,0 +1,16 @@
+/*
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#ifndef __SYS_VT_h
+#define __SYS_VT_h
+
+/* XXX 
+ * Add constants and missing routines indicated by compiling svgalib.
+ */
+
+#endif
