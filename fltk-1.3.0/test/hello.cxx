46c28a161b2dbae75324893ea09e61fe266bf4bc
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2012-07-08 16:00:23 +0300

    Hello World building and running for rtems

--- fltk-1.3.0/test/hello.cxx
+++ fltk-1.3.0/test/hello.cxx
@@ -29,13 +29,13 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 
-int main(int argc, char **argv) {
+extern "C" int rtems_main(int argc, char **argv) {
   Fl_Window *window = new Fl_Window(340,180);
   Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
   box->box(FL_UP_BOX);
   box->labelfont(FL_BOLD+FL_ITALIC);
   box->labelsize(36);
-  box->labeltype(FL_SHADOW_LABEL);
+  box->labeltype(FL_NORMAL_LABEL);
   window->end();
   window->show(argc, argv);
   return Fl::run();
