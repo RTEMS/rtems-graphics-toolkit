532c0192bcafcfb6fc296d9a4aaae272d530d42f
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2012-08-09 16:22:52 +0300

    Added comment to rtems_main.cxx

--- fltk-1.3.0/rtems_main.cxx
+++ fltk-1.3.0/rtems_main.cxx
@@ -1,21 +1,8 @@
-//#ifndef RTEMS_MAIN_H
-//#define RTEMS_MAIN_H
+/* Added to function as a wrapper from 
+ * nano-X rtems_main call and usual main entry-point
+ */
 
-//transform call to main to call to rtems_main
-//#define main(x,y) rtems_main(x,y)
-//#define main(x)   rtems_main(x)
-//#define main()    rtems_main()
-
-//extern "C" int rtems_main(int argc, char **argv)
-//{
-//  main(argc, argv);
-//}
-//
-//
-//#endif
-//extern "C" 
 extern int main(int argc, char **argv);
-
 
 extern "C" int rtems_main(int argc, char **argv)
 {
